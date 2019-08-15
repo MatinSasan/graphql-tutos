301cc6da4f18f7dac60fbd1c2cb937dea3565f3a
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-15 17:57:28 +0430

    more complex query

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,9 +1,32 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// Demo User data
+
+const users = [
+  {
+    id: '1',
+    name: 'Matin',
+    email: 'lol@loland.com',
+    age: 27
+  },
+  {
+    id: '2',
+    name: 'Mobin',
+    email: 'lol2@loland.com',
+    age: 26
+  },
+  {
+    id: '3',
+    name: 'nobody',
+    email: 'ghost@ghost.com'
+  }
+];
+
 // type definition (schema)
 const typeDefs = `
 
 type Query {
+  users(query: String): [User!]!
   me: User!
   post: Post!
   greeting(name: String, hobby: String): String!
@@ -28,6 +51,14 @@ type Post {
 // Resolvers
 const resolvers = {
   Query: {
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
     greeting(parent, args, ctx, info) {
       if (args.name && args.hobby) {
         return `Hello ${args.name} and I love ${args.hobby} :D`;
