c1263dffb48a80ae8d48b01e9603913a5459b701
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-17 17:40:56 +0430

    added update functionality for user,  comment and post

--- basics/src/resolvers/Mutation.js
+++ basics/src/resolvers/Mutation.js
@@ -16,6 +16,53 @@ const Mutation = {
     db.users.push(user);
     return user;
   },
+  deleteUser(parent, args, { db }, info) {
+    const userIndex = db.users.findIndex(user => user.id === args.id);
+
+    if (userIndex === -1) {
+      throw new Error('User not found');
+    }
+
+    const deletedUser = db.users.splice(userIndex, 1);
+
+    db.posts = db.posts.filter(post => {
+      const match = post.author === args.id;
+
+      if (match) {
+        db.comments = db.comments.filter(comment => comment.post !== post.id);
+      }
+
+      return !match;
+    });
+
+    db.comments = db.comments.filter(comment => comment.author !== args.id);
+
+    return deletedUser[0];
+  },
+  updateUser(parent, args, { db }, info) {
+    const { id, data } = args;
+    const user = db.users.find(user => user.id === id);
+
+    if (!user) throw new Error('User not found');
+
+    if (typeof data.email === 'string') {
+      const emailTaken = db.users.some(user => user.email === data.email);
+
+      if (emailTaken) throw new Error('Email taken');
+
+      user.email = data.email;
+    }
+
+    if (typeof data.name === 'string') {
+      user.name = data.name;
+    }
+
+    if (typeof data.age !== 'undefined') {
+      user.age = data.age;
+    }
+
+    return user;
+  },
   createPost(parent, args, { db }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
 
@@ -29,6 +76,35 @@ const Mutation = {
     };
 
     db.posts.push(post);
+
+    return post;
+  },
+  deletePost(parent, args, { db }, info) {
+    const postIndex = db.posts.findIndex(post => post.id === args.id);
+
+    if (postIndex === -1) throw new Error('Post not found');
+
+    const deletedPost = db.posts.splice(postIndex, 1);
+
+    return deletedPost[0];
+  },
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
+    const post = db.posts.find(post => post.id === id);
+
+    if (!post) throw new Error('Post not found');
+
+    if (typeof data.title === 'string') {
+      post.title = data.title;
+    }
+
+    if (typeof data.body === 'string') {
+      post.body = data.body;
+    }
+
+    if (typeof data.published === 'boolean') {
+      post.published = data.published;
+    }
 
     return post;
   },
@@ -62,28 +138,16 @@ const Mutation = {
 
     return deletedComments[0];
   },
-  deleteUser(parent, args, { db }, info) {
-    const userIndex = db.users.findIndex(user => user.id === args.id);
+  updateComment(parent, { id, data }, { db }, info) {
+    const comment = db.comments.find(comment => comment.id === id);
 
-    if (userIndex === -1) {
-      throw new Error('User not found');
+    if (!comment) throw new Error('No comment found');
+
+    if (typeof data.text === 'string') {
+      comment.text = data.text;
     }
 
-    const deletedUser = db.users.splice(userIndex, 1);
-
-    db.posts = db.posts.filter(post => {
-      const match = post.author === args.id;
-
-      if (match) {
-        db.comments = db.comments.filter(comment => comment.post !== post.id);
-      }
-
-      return !match;
-    });
-
-    db.comments = db.comments.filter(comment => comment.author !== args.id);
-
-    return deletedUser[0];
+    return comment;
   }
 };
 
