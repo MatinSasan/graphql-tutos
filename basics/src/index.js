f5912b22c4e068fdff3bd60aa75665440aca5f63
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-18 17:34:46 +0430

    added subscription | edited Schema, index, and added subscription.js

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,6 +1,8 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
+
 import User from './resolvers/User';
 import Post from './resolvers/Post';
 import Comment from './resolvers/Comment';
@@ -12,15 +14,18 @@ import db from './fakeData';
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   Post,
   User,
   Comment
 };
 
+const pubsub = new PubSub();
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { db }
+  context: { db, pubsub }
 });
 
 server.start(({ port }) => {
