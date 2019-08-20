4eb6ab4127b60714e5b6fcdcc4774cd4ffccc622
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-20 17:34:29 +0430

    fixed typos in src/prisma.js | testing accessing prisma query in prisma.js

--- prisma/src/prisma.js
+++ prisma/src/prisma.js
@@ -1,6 +1,14 @@
-import {Prisma} from 'prisma-binding';
+import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-  typeDefs: 'src/generated/prisma.grahpql',
-  endpoint: 'localhost:4466'
-})
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: 'http://localhost:4466'
+});
+
+prisma.query.users(null, '{ id name email posts { id title }}').then(data => {
+  console.log(JSON.stringify(data, undefined, 2));
+});
+
+prisma.query.comments(null, '{id author text}').then(data => {
+  console.log(JSON.stringify(data, undefined, 2));
+});
