a7c4a6088a26233c817a47ac7e6809861650efd8
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-20 15:54:46 +0430

    added node to prisma project | added two dependencies: prisma-binding graphql-cli | generated prisma.graphql | added prisma.js to src

---
+++ prisma/src/fakeData.js
@@ -0,0 +1,78 @@
+const users = [
+  {
+    id: '1',
+    name: 'Matin',
+    email: 'lol@loland.com',
+    age: 27
+  },
+  {
+    id: '2',
+    name: 'Mobin',
+    email: 'lol2@loland.com',
+    age: 26
+  },
+  {
+    id: '3',
+    name: 'nobody',
+    email: 'ghost@ghost.com'
+  }
+];
+
+const posts = [
+  {
+    id: '10',
+    title: 'learning graphql',
+    body: 'have to fill this body',
+    published: true,
+    author: '1'
+  },
+  {
+    id: '11',
+    title: 'feeling graphql',
+    body: 'have to fill this body again',
+    published: true,
+    author: '2'
+  },
+  {
+    id: '12',
+    title: 'mastering graphql',
+    body: 'mastering something takes time',
+    published: true,
+    author: '3'
+  }
+];
+
+const comments = [
+  {
+    id: '101',
+    text: 'this is some stupid comment',
+    author: '3',
+    post: '10'
+  },
+  {
+    id: '102',
+    text: 'this is some smartass comment',
+    author: '2',
+    post: '11'
+  },
+  {
+    id: '103',
+    text: 'this is some empty comment',
+    author: '1',
+    post: '10'
+  },
+  {
+    id: '104',
+    text: 'this is some lovely comment',
+    author: '3',
+    post: '12'
+  }
+];
+
+const db = {
+  users,
+  posts,
+  comments
+};
+
+export { db as default };
---
+++ prisma/src/index.js
@@ -0,0 +1,33 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
+
+import User from './resolvers/User';
+import Post from './resolvers/Post';
+import Comment from './resolvers/Comment';
+
+// Demo User data
+import db from './fakeData';
+
+// Resolvers
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  Post,
+  User,
+  Comment
+};
+
+const pubsub = new PubSub();
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: { db, pubsub }
+});
+
+server.start(({ port }) => {
+  console.log(`Server is up at port ${port}`);
+});
