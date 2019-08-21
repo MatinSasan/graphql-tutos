c1266a1e071418f1b94af1f19b4442effabb9f58
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-21 14:56:14 +0430

    added more complexity to the functions in prisma.js

--- prisma/src/prisma.js
+++ prisma/src/prisma.js
@@ -13,7 +13,17 @@ const prisma = new Prisma({
 //   console.log(JSON.stringify(data, undefined, 2));
 // });
 
+// prisma.exists
+//   .Comment({
+//     id: 'cjzjo8hlp00ts07157ybepz8j'
+//   })
+//   .then(exists => console.log(exists));
+
 const createPostForUser = async (authorId, data) => {
+  const userExists = await prisma.exists.User({ id: authorId });
+
+  if (!userExists) throw new Error('No user found');
+
   const post = await prisma.mutation.createPost(
     {
       data: {
@@ -25,22 +35,27 @@ const createPostForUser = async (authorId, data) => {
         }
       }
     },
-    '{ id }'
+    '{ author {id name email posts {id title published}} }'
   );
 
-  const user = await prisma.query.user(
-    {
-      where: {
-        id: authorId
-      }
-    },
-    '{ id name email posts { id title published } }'
-  );
+  // const user = await prisma.query.user(
+  //   {
+  //     where: {
+  //       id: authorId
+  //     }
+  //   },
+  //   '{ id name email posts { id title published } }'
+  // );
 
-  return user;
+  // return user;
+  return post;
 };
 
 const updatePostForUser = async (postId, data) => {
+  const postExists = await prisma.exists.Post({ id: postId });
+
+  if (!postExists) throw new Error('No post found');
+
   const post = await prisma.mutation.updatePost(
     {
       where: {
@@ -48,29 +63,35 @@ const updatePostForUser = async (postId, data) => {
       },
       data
     },
-    '{ author { id } }'
+    '{ author { id name email posts {id title published} } }'
   );
 
-  const user = await prisma.query.user(
-    {
-      where: {
-        id: post.author.id
-      }
-    },
-    '{ id name email posts { id title published }}'
-  );
+  // const user = await prisma.query.user(
+  //   {
+  //     where: {
+  //       id: post.author.id
+  //     }
+  //   },
+  //   '{ id name email posts { id title published }}'
+  // );
 
-  return user;
+  // return user;
+
+  return post.author;
 };
+
+// ===== COMMANDS =====
 
 // createPostForUser('cjzjlnqlm00700715edug3ak7', {
 //   title: 'great books',
 //   body: 'Wisdom of Life',
 //   published: true
-// }).then(user => {
-//   console.log(JSON.stringify(user, undefined, 2));
-// });
+// })
+//   .then(user => {
+//     console.log(JSON.stringify(user, undefined, 2));
+//   })
+//   .catch(err => console.log(err));
 
-updatePostForUser('cjzl18h7800f407159r0fw3g2', { published: false }).then(
-  user => console.log(JSON.stringify(user, undefined, 2))
-);
+updatePostForUser('cjzl18h7800f407159r0fw3g2', { published: false })
+  .then(user => console.log(JSON.stringify(user, undefined, 2)))
+  .catch(err => console.log(err.message));
