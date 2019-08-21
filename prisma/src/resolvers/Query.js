a7763c7f2b2890624c61a6935e4a11c6f3a0d5a3
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-21 23:29:01 +0430

    integrated operation arguments | removed old methods

--- prisma/src/resolvers/Query.js
+++ prisma/src/resolvers/Query.js
@@ -1,27 +1,42 @@
 const Query = {
   users(parent, args, { prisma }, info) {
-    return prisma.query.users(null, info);
+    const opArgs = {};
 
-    // if (!args.query) {
-    //   return db.users;
-    // }
-    // return db.users.filter(user => {
-    //   return user.name.toLowerCase().includes(args.query.toLowerCase());
-    // });
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          {
+            name_contains: args.query
+          },
+          {
+            email_contains: args.query
+          }
+        ]
+      };
+    }
+
+    return prisma.query.users(opArgs, info);
   },
   posts(parent, args, { prisma }, info) {
-    return prisma.query.posts(null, info);
+    const opArgs = {};
 
-    // if (!args.query) {
-    //   return db.posts;
-    // }
-    // return db.posts.filter(user => {
-    //   return post.name.toLowerCase().includes(args.query.toLowerCase());
-    // });
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          {
+            title_contains: args.query
+          },
+          {
+            body_contains: args.query
+          }
+        ]
+      };
+    }
+
+    return prisma.query.posts(opArgs, info);
   },
   comments(parent, args, { prisma }, info) {
     return prisma.query.comments(null, info);
-    // return db.comments;
   }
 };
 
