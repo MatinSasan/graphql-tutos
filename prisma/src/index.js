da321727df6e31ccf408b93b99dc86a78b82eb9d
Matin Sasanpour <matin.sasan@gmail.com>
2019-09-07 23:37:16 +0430

    fragments added for User query

--- prisma/src/index.js
+++ prisma/src/index.js
@@ -1,23 +1,9 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga';
-import Query from './resolvers/Query';
-import Mutation from './resolvers/Mutation';
-import Subscription from './resolvers/Subscription';
 
-import User from './resolvers/User';
-import Post from './resolvers/Post';
-import Comment from './resolvers/Comment';
 import prisma from './prisma';
 
 // Resolvers
-const resolvers = {
-  Query,
-  Mutation,
-  Subscription,
-  User,
-  Post,
-  Comment
-};
-
+import { resolvers, fragmentReplacements } from './resolvers/index';
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
@@ -25,7 +11,8 @@ const server = new GraphQLServer({
   resolvers,
   context(req) {
     return { pubsub, prisma, req };
-  }
+  },
+  fragmentReplacements
 });
 
 server.start(({ port }) => {
--- prisma/src/resolvers/User.js
+++ prisma/src/resolvers/User.js
@@ -1,13 +1,29 @@
 import getUserId from '../utils/getUserId';
 
 const User = {
-  email(parent, args, { req }, info) {
-    const userId = getUserId(req, false);
+  email: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, { req }, info) {
+      const userId = getUserId(req, false);
 
-    if (userId && userId === parent.id) {
-      return parent.email;
-    } else {
-      return null;
+      if (userId && userId === parent.id) {
+        return parent.email;
+      } else {
+        return null;
+      }
+    }
+  },
+  posts: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, { prisma }, info) {
+      return prisma.query.posts({
+        where: {
+          published: true,
+          author: {
+            id: parent.id
+          }
+        }
+      });
     }
   }
 };
