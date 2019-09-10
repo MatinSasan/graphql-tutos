858c577ba5133dde8b522130df76f910ed94f95e
Matin Sasanpour <matin.sasan@gmail.com>
2019-09-10 20:28:24 +0430

    further testing added | graphql error causes test to fail...for now

--- prisma/tests/user.test.js
+++ prisma/tests/user.test.js
@@ -1,3 +1,33 @@
-test('dummy test', () => {
-  1 + 2;
+import 'cross-fetch/polyfill';
+import '@babel/polyfill';
+// require('@babel/polyfill/noConflict'); //this was required for 'ReferenceError: regeneratorRuntime'
+import ApolloBoost, { gql } from 'apollo-boost';
+import prisma from '../src/prisma';
+
+const client = new ApolloBoost({
+  uri: 'http://localhost:4000'
+});
+
+test('Should create a new user', async () => {
+  const createUser = gql`
+    mutation {
+      createUser(
+        data: { name: "Mat", email: "lol6@lol.com", password: "12345" }
+      ) {
+        token
+        user {
+          id
+        }
+      }
+    }
+  `;
+
+  const res = await client.mutate({
+    mutation: createUser
+  });
+
+  const itExists = await prisma.exists.User({
+    id: res.data.createUser.user.id
+  });
+  expect(itExists).toBe(true);
 });
