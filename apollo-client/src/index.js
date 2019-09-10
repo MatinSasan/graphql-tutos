00bc553d513931193137965194b3cd7e71e1f25b
Matin Sasanpour <matin.sasan@gmail.com>
2019-09-10 14:09:21 +0430

    basic querying data from users and posts | apollo-boost and graphql added as dependencies

--- apollo-client/src/index.js
+++ apollo-client/src/index.js
@@ -1,1 +1,51 @@
-console.log('test');
+import ApolloBoost, { gql } from 'apollo-boost';
+
+const client = new ApolloBoost({
+  uri: 'http://localhost:4000'
+});
+
+const getUsers = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`;
+
+client.query({ query: getUsers }).then(res => {
+  let html = '';
+
+  res.data.users.forEach(user => {
+    html += `
+        <div>
+          <h1>${user.name}</h1>
+        </div>
+      `;
+  });
+
+  document.getElementById('users').innerHTML = html;
+});
+
+const getPosts = gql`
+  query {
+    posts {
+      title
+      author {
+        name
+      }
+    }
+  }
+`;
+
+client.query({ query: getPosts }).then(res => {
+  let html = '';
+  res.data.posts.forEach(post => {
+    html += `
+      <div>
+        <h1>${post.title}</h1>
+      </div>
+    `;
+  });
+  document.getElementById('posts').innerHTML = html;
+});
